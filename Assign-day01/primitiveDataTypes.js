668777490b801db0224c940fa8a6e3d093e8a586
PavithraMurugesan-123 <pavithramurugesan007@zohomail.in>
2025-10-14 14:28:38 +0530

    Week1-day01

---
+++ Assign-day01/primitiveDataTypes.js
@@ -0,0 +1,11 @@
+//Primitive Data Types
+let userName="Pavithra" // String
+console.log(typeof(userName)+" "+userName)
+let organization="ABC" // String
+console.log(typeof(organization)+" "+organization)
+let contactNumber=9944686388 // Number
+console.log(typeof(contactNumber)+" "+contactNumber)
+let knowsAutomation=true // Boolean
+console.log(typeof(knowsAutomation)+" "+knowsAutomation)
+let usesPlaywright // Undefined (not assigned)
+console.log(typeof(usesPlaywright)+" "+usesPlaywright)
